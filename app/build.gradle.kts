04fee97d1425120d163f5b6b50428155bd2d1369
yahir <s20120196@alumnos.itsur.edu.mx>
2023-10-01 12:19:17 -0600

    La dependencia implementation("androidx.core:core-ktx:") se actualizo a la version más actual, tambien se modifico el compileSdk a la version 34 y por ultimo se añadio la fuenta para trabajar con el proyecto, la cual se llama "caliban.ttf"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+
 }
 
 android {
     namespace = "com.juanyahirdr.proyectofinal"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.juanyahirdr.proyectofinal"
@@ -51,7 +52,8 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    //DEPENDENCIAS DEFAULT
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
