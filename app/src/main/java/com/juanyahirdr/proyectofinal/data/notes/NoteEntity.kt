2f4a5dc8cbb6e76fa45628335c8780c23269995c
yahir <s20120196@alumnos.itsur.edu.mx>
2023-10-01 14:20:08 -0600

    Se creo una clase llamda NoteEntity para usar cpn el dataClass, en donde contrenda los querys para usar en la base de datos

--- app/src/main/java/com/juanyahirdr/proyectofinal/data/notes/NoteEntity.kt
+++ app/src/main/java/com/juanyahirdr/proyectofinal/data/notes/NoteEntity.kt
@@ -5,7 +5,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 //DATACLASS ES USADO POR KOTLIN PARA GUARDAR DATOS
-@Entity(tableName= "note")
+@Entity(tableName= "note")  //specificar el nombre de la tabla en la que se almacenarán los objetos de esta entidad en la base de datos.
 data class NoteEntity( //Se esta creando una entidad para las notas
     @PrimaryKey val uid: Int,   //ID PARA SABER QUE NOTA ES
     //columnas:
