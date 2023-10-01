cf61be3f8903aec36e092bad9c334cd22b5d47b3
yahir <s20120196@alumnos.itsur.edu.mx>
2023-10-01 14:37:11 -0600

    Se agro un nuevo metodo en el DAO el cual es @Update, ademas de modificar Insert oara que no sea InsertAll --> Insert

--- app/src/main/java/com/juanyahirdr/proyectofinal/data/notes/NoteDao.kt
+++ app/src/main/java/com/juanyahirdr/proyectofinal/data/notes/NoteDao.kt
@@ -4,16 +4,21 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.juanyahirdr.proyectofinal.utils.Constants
 
 @Dao
 interface NoteDao {
+    //PERMITE CONSULTAR LA BASE DE DATOS
     @Query("SELECT * FROM ${Constants.note_database_table_name}")
     fun getAll(): List<NoteEntity>
 
     @Insert //SIRVE PARA INSERTAR LA NOTA A LA BASE DE DATOS
-    fun insertAll(noteEntity: NoteEntity)
+    fun insert(noteEntity: NoteEntity)
 
     @Delete //SIRVE PARA BORRAR LA NOTA EN LA BASE DE DATOS
     fun delete(noteEntity: NoteEntity)
+
+    @Update //SIRVE PARA ACTUALIZAR UNA NOTA
+    fun update(noteEntity: NoteEntity)
 }
