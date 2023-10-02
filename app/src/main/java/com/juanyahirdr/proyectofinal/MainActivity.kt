96c645861a52d1215df451293475aa18706456c3
yahir <s20120196@alumnos.itsur.edu.mx>
2023-10-02 14:33:56 -0600

    Se agrego una funcion que contiene los botones, con esto se concluye el prototipo de crear una nota

--- app/src/main/java/com/juanyahirdr/proyectofinal/MainActivity.kt
+++ app/src/main/java/com/juanyahirdr/proyectofinal/MainActivity.kt
@@ -5,11 +5,13 @@ import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -23,19 +25,21 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.juanyahirdr.proyectofinal.ui.theme.ProyectoFinalTheme
 import org.w3c.dom.Text
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ProyectoFinalTheme{
-                textsFields()
+                createNote()
             }
         }
     }
@@ -43,12 +47,12 @@ class MainActivity : ComponentActivity() {
 //VERSION DE PUEBRA
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable //INDICA QUE ES UN ELEMENTO GRAFICO
-fun textsFields(){
-
+fun createNote(){
+    //VARIABLES:
     var titulo by remember { mutableStateOf("") }
     var body by remember { mutableStateOf("") }
-    var currentTitulo by remember{ mutableStateOf("")}
-    var currentBody by remember{ mutableStateOf("")}
+
+
     Column(modifier = Modifier
         .fillMaxWidth()
         .padding(16.dp)) {
@@ -57,24 +61,33 @@ fun textsFields(){
         Spacer(modifier = Modifier.height(16.dp))
         Text(text= "Cuerpo:")
         TextField(value = body, onValueChange = { body=it})
-        Button(onClick = {
-            if (!currentTitulo.isBlank() and !currentBody.isBlank()){
-                currentTitulo = titulo
-                currentBody = body
-            }
-        }) {
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Fecha:")
+        buttuns()
+
+    }
+}
+@Composable
+fun buttuns() {
+    Row( modifier = Modifier.padding(10.dp)) {
+        Button(onClick = { /*TODO*/ }) {
             Text(text = "Guardar")
         }
+        Spacer(modifier = Modifier.width(10.dp))//añade una separacion
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Cancelar")
+        }
+        Spacer(modifier = Modifier.width(10.dp))
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Eliminar")
+        }
     }
-
 }
-
-
 
 //La preview sirve para ver el estado de la app sin tener que iniciar el emulador
 @Preview
 @Composable
 fun PreviewText(){
     //INIDICAR QUE SE QUIERE PREVISUALIZAR
-    textsFields()
+    createNote()
 }
