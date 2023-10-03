5876e753b213078077b7e6ccc37509998fd4ae35
yahir <s20120196@alumnos.itsur.edu.mx>
2023-10-03 17:53:56 -0600

    Se establecio la fuente en Type

--- app/src/main/java/com/juanyahirdr/proyectofinal/ui/theme/Theme.kt
+++ app/src/main/java/com/juanyahirdr/proyectofinal/ui/theme/Theme.kt
@@ -64,7 +64,7 @@ fun ProyectoFinalTheme(
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
-    
+
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
--- app/src/main/java/com/juanyahirdr/proyectofinal/ui/theme/Type.kt
+++ app/src/main/java/com/juanyahirdr/proyectofinal/ui/theme/Type.kt
@@ -2,9 +2,15 @@ package com.juanyahirdr.proyectofinal.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.juanyahirdr.proyectofinal.R
+//FUENTE
+private val caliban = FontFamily(
+    Font(R.font.caliban)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
