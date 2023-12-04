93f2817d1ab309a46d6d86a825b55946d853ce56
yahir <s20120196@alumnos.itsur.edu.mx>
2023-12-04 16:28:26 -0600

    Se modifico las notificaciones, ya permite seleccionar las notas

--- ProyectoNotas/app/src/main/java/com/example/proyectofinal_jma/model/Content.kt
+++
@@ -1,7 +0,0 @@
-package com.example.proyectofinal_jma.model
-
-import androidx.annotation.StringRes
-
-data class Content(
-    @StringRes val text:Int,
-)
--- ProyectoNotas/app/src/main/java/com/example/proyectofinal_jma/navigation/NavigationDestination.kt
+++
@@ -1,6 +0,0 @@
-package com.example.proyectofinal_jma.navigation
-
-interface NavigationDestination {
-    val route: String
-    val titleRes: Int
-}
